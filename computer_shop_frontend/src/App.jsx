7a33a4066a55e61a7bfed5ff4567ee45ced3a204
Magallanes Alfonso <a.magallanes@etec.local>
2023-08-16 12:08:13 -0300

    Login-Singin

--- computer_shop_frontend/src/App.jsx
+++ computer_shop_frontend/src/App.jsx
@@ -5,14 +5,19 @@ import './App.css';
 import Header from './Components/header/header.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import MainSection from './Components/MainSection/MainSection';
-
+import Login from './Components/Login/Login';
+import Singin from './Components/Singin/Singin'
 
 function App() {
   return (
     <div className="App">
        <Header></Header>
        <MainSection></MainSection>
-       <Footer></Footer>
+       <Footer></Footer> 
+       {/* <Login></Login>
+       <Singin></Singin> */}
+
+       
     </div>
   );
 }
---
+++ computer_shop_frontend/src/Components/Login/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Login.css'
+
+const Login = () => {
+    return (
+                <div>
+                    <head>
+                <meta charset="UTF-8"/>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Teko:wght@600&display=swap" rel="stylesheet"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Tamil:wght@200&display=swap" rel="stylesheet"/>
+                <title>Computer shop</title>
+            </head>
+                    <div class="login-container">
+                <h2>Iniciar sesión</h2>
+                <form action="procesar-login.php" method="POST">
+                    <div class="form-group">
+                        <label for="username">Usuario:</label>
+                        <input type="text" id="username" name="username" required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Contraseña:</label>
+                        <input type="password" id="password" name="password" required/>
+                    </div>
+                    <button type="submit">Iniciar sesión</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
